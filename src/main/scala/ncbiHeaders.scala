17fd783020af8b00dff99707ca10f1aa59b087ae
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2016-02-19 17:18:59 +0100

    add a really stupid ncbi ids representation

---
+++ src/main/scala/ncbiHeaders.scala
@@ -0,0 +1,53 @@
+package ohnosequences.fastarious
+
+import ohnosequences.cosas._, types._, records._, fns._, klists._
+
+case object ncbiHeaders {
+
+  trait NcbiID extends AnyType
+  // TODO all instances here
+  case object gi    extends Type[Int]("gi") with NcbiID
+  case object gb    extends Type[accession]("gb") with NcbiID
+  case object name  extends Type[String]("") with NcbiID
+
+  class accession(val acc: String, val locus: String) {
+
+    override def toString: String = s"${acc}|${locus}"
+  }
+
+  case object accession {
+
+    def apply(acc: String, locus: String): accession = new accession(acc,locus)
+  }
+
+  case object ncbiHeader extends RecordType(gb :×: gi :×: name :×: |[AnyType]) {
+
+    type RealRaw = (gb.type := gb.Raw) :: (gi.type := gi.Raw) :: (name.type := name.Raw) :: *[AnyDenotation]
+
+    implicit def buh(v: ncbiHeader.type := ncbiHeader.RealRaw): ncbiHeaderOps = ncbiHeaderOps(v.value)
+  }
+
+  case object toHeader extends DepFn1[Any,String] {
+
+    // first one, no need to add a '|'
+    implicit val atFirst: AnyApp1At[toHeader.type, gb.type := accession] { type Y = String } =
+      toHeader at { iv => s"${iv.tpe.label}|${utils.removeAllSpace(iv.value.toString)}" }
+
+    // for all the others, add '|' first
+    implicit def default[I <: NcbiID, V <: I#Raw]: AnyApp1At[toHeader.type, I := V] { type Y = String } =
+      toHeader at { iv: I := V => s"|${iv.tpe.label}|${utils.removeAllSpace(iv.value.toString)}" }
+
+    // but for 'name', which just needs a space I don't know why
+    implicit val atName: AnyApp1At[toHeader.type, name.type := String] { type Y = String } =
+      toHeader at { u => s" ${u.value}" }
+  }
+
+  case object ncbiHeaderOps {
+
+
+  }
+  case class ncbiHeaderOps(v: ncbiHeader.RealRaw) extends AnyVal {
+
+    def asFastaHeader: fasta.header.type := fasta.FastaHeader = fasta.header(fasta.FastaHeader( v.map(toHeader).asList.mkString("") ))
+  }
+}
---
+++ src/test/scala/NcbiHeadersTests.scala
@@ -0,0 +1,21 @@
+package ohnosequences.fastarious.test
+
+import org.scalatest.FunSuite
+
+import ohnosequences.cosas._, types._, klists._, records._
+import ohnosequences.fastarious._, fasta._, ncbiHeaders._
+
+class NcbiHeadersTests extends FunSuite {
+
+  val ramdonIds = ncbiHeader(
+    gb(accession("A3CFTC4.4", "X4CC8HG"))           ::
+    gi(21312324)                                    ::
+    name("A really interesting sequence hola hola") ::
+    *[AnyDenotation]
+  )
+
+  test("can construct a header string from an ncbi record value") {
+
+    println { ramdonIds.asFastaHeader.value.asString }
+  }
+}
